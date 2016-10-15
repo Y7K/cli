34559763a0a3bd0c6f0ccd2d96f84e36131d56d4
Joris Noordermeer <jo.noordermeer@gmail.com>
2016-10-15 12:58:56 +0200

    Allow extracting multiple subfolders of the zip

--- src/Command.php
+++ src/Command.php
@@ -71,7 +71,7 @@ class Command extends \Symfony\Component\Console\Command\Command
 
     }
 
-    protected function unzip($zip, $path)
+    protected function unzip($zip, $path, $subfolders = null)
     {
 
         // build the temporary folder path
@@ -83,23 +83,29 @@ class Command extends \Symfony\Component\Console\Command\Command
         // get the list of directories within our tmp folder
         $dirs = glob($tmp . '/*');
 
-        // get the source directory from the tmp folder
-        if (isset($dirs[0]) && is_dir($dirs[0])) {
-            $source = $dirs[0];
-        } else {
-            throw new RuntimeException('The source directory could not be found');
-        }
+        if (!is_array($subfolders)) $subfolders = [$subfolders];
 
-        // create the folder if it does not exist yet
-        if (!is_dir($path)) mkdir($path);
+        // Loop through all subfolders and extract them
+        foreach ($subfolders as $subfolder) {
 
-        // extract the content of the directory to the final path
-        foreach ((array)array_diff(scandir($source), ['.', '..']) as $name) {
-            if (!rename($source . '/' . $name, $path . '/' . $name)) {
-                throw new RuntimeException($name . ' could not be copied');
+            $source = $dirs[0] . '/' . $subfolder;
+
+            // get the source directory from the tmp folder
+            if (!isset($source) || !is_dir($source)) {
+                throw new RuntimeException('The source directory could not be found');
             }
-        }
 
+            // create the folder if it does not exist yet
+            if (!is_dir($path)) mkdir($path);
+
+            // extract the content of the directory to the final path
+            foreach ((array)array_diff(scandir($source), ['.', '..']) as $name) {
+                if (!rename($source . '/' . $name, $path . '/' . $name)) {
+                    throw new RuntimeException($name . ' could not be copied');
+                }
+            }
+
+        }
         // remove the zip file
         Util::remove($zip);
 
