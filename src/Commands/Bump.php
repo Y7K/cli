076d22ad70a5c02fa8079af6f9119c2f486b63b6
Joris Noordermeer <jo.noordermeer@gmail.com>
2016-11-03 10:45:12 +0100

    Update Bump command to set trailing Numbers to Zero

--- src/Commands/Bump.php
+++ src/Commands/Bump.php
@@ -24,18 +24,22 @@ class Bump extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
+
+        // Check if Porjec.json file exists
         $projectFile = $this->dir() . '/project.json';
 
         if (!file_exists($projectFile)) {
             throw new RuntimeException('Project.json File not found!');
         }
 
+        // Read project.json file
         $projectData = json_decode(file_get_contents($projectFile));
 
         if (!isset($projectData->version)) {
             throw new RuntimeException('No version specified in project.json file!');
         }
 
+        // Get the version to update
         $version = strtolower($input->getArgument('version'));
         $semver = ['major', 'minor', 'patch'];
 
@@ -43,13 +47,24 @@ class Bump extends Command
             throw new RuntimeException('Version Argument must be \'major\', \'minor\' or \'patch\'');
         }
 
+        // Extract Version to Array
         $projectVersion = explode('.',$projectData->version);
         $update = array_search($version, $semver);
 
+        // Increase selected Number
         $projectVersion[$update] = (int) $projectVersion[$update] + 1;
 
+        // Set Trailing numbers to Zero
+        if($update<count($projectVersion)) {
+            foreach (range($update + 1,count($projectVersion)) as $v) {
+                $projectVersion[$v] = 0;
+            }
+        }
+
+        // Save the updated Versin
         $projectData->version = implode('.', $projectVersion);
 
+        // Write To file
         file_put_contents($projectFile, json_encode($projectData, JSON_PRETTY_PRINT));
 
         $output->writeln('<info>Version updated to ' . $projectData->version . '</info>');
