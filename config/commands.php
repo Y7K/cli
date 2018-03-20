9998b048c7b5d1428c040ae721a688822cadebca
Joris Noordermeer <jo.noordermeer@gmail.com>
2018-03-20 13:59:19 +0100

    hide install commands

--- config/commands.php
+++ config/commands.php
@@ -57,7 +57,15 @@ return [
         Symfony\Component\Console\Command\HelpCommand::class,
         Illuminate\Console\Scheduling\ScheduleRunCommand::class,
         Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
-        Illuminate\Foundation\Console\VendorPublishCommand::class
+        Illuminate\Foundation\Console\VendorPublishCommand::class,
+
+        // Install commands
+        \App\Commands\Install\Platforms\InstallCraft2Command::class,
+        \App\Commands\Install\Platforms\InstallCraft3Command::class,
+        \App\Commands\Install\Platforms\InstallLaravelCommand::class,
+        \App\Commands\Install\Platforms\InstallPlainCommand::class,
+        \App\Commands\Install\Resources\InstallJavascriptCommand::class,
+        \App\Commands\Install\Resources\InstallStylesheetsCommand::class,
     ],
 
     /*
