dbaeaf42fed3cce8d735cc30b43c4ba39a2a3b8b
Joris Noordermeer <jo.noordermeer@gmail.com>
2018-08-08 13:58:28 +0200

    Updates Name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'name' => 'Y7K',
+    'name' => 'Y7K CLI',
 
     /*
     |--------------------------------------------------------------------------
